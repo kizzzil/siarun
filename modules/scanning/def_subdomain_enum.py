d34a51ea3c2ddf607f61c85c69f121e1d99f3c6e
kizzzil <kizzzil5@gmail.com>
2024-05-28 19:59:48 +0400

    add subdomain_enum

--- modules/scanning/def_subdomain_enum.py
+++ modules/scanning/def_subdomain_enum.py
@@ -1,5 +1,58 @@
 import json
+import requests
+from requests.exceptions import HTTPError
 
-with open("../../target.json", 'r') as file:
-    data = json.load(file)
+def subdomain_enum(site, wordlist_path):
+    valid_subdomain = []
 
+    with open(wordlist_path, 'r') as wordlist:
+        list_subdomain_prefix = wordlist.readlines()
+        # орабатываем конец строки \n в wordlist
+        for i_prefix in range(len(list_subdomain_prefix)):
+            if list_subdomain_prefix[i_prefix][-1] == '\n':
+                list_subdomain_prefix[i_prefix] = list_subdomain_prefix[i_prefix][:-1:]
+
+    for prefix in list_subdomain_prefix:
+        http_err = False 
+        https_err = False 
+
+        # попытка подключения по http
+        try:
+            print(f"connect to http://{prefix}.{site}")
+            response_http = requests.get(f"http://{prefix}.{site}")
+        except (HTTPError, ConnectionError, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+            print('HTTP ERROR')
+            http_err = True
+
+        # попытка подключения по https
+        try:
+            print(f"connect to https://{prefix}.{site}")
+            response_https = requests.get(f"http://{prefix}.{site}")
+        except (HTTPError, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+            print('HTTPs ERROR')
+            https_err = True
+            
+        if not(http_err):
+            valid_subdomain.append(f"http://{prefix}.{site}")
+        if not(https_err):
+            valid_subdomain.append(f"https://{prefix}.{site}")
+
+    return valid_subdomain
+
+def main():
+    # десериализация из json
+    with open('target.json', 'r') as file:
+        data = json.load(file)
+
+
+    valid_subdomain = subdomain_enum(data['target'], \
+                                     data['settings']['subdomain_wordlist'])
+    data['subdomains'] = valid_subdomain
+
+    # valid_subdomain.append(data['target'])
+    # сериализация в json
+    with open('target.json', 'w') as file:
+        json.dump(data, file, indent=4)
+        
+
+main()
